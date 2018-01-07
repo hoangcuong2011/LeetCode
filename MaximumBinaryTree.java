3e77493fb8381de587073ce7d9ffd8933683a4a6
Hoang Cuong <hoangcuong2011@gmail.com>
2018-01-07 13:39:34 -0500

    Create MaximumBinaryTree.java

---
+++ MaximumBinaryTree.java
@@ -0,0 +1,25 @@
+//The problem: https://leetcode.com/problems/maximum-binary-tree/description/
+public TreeNode constructMaximumBinaryTree(int[] nums) {
+		TreeNode head = null;
+        head = constructMaximumBinaryTree(nums, 0, nums.length, head);
+        return head;
+    }
+    public TreeNode constructMaximumBinaryTree(int[] nums, int leftIndex, int rightIndex, TreeNode head) {
+    	if(leftIndex==rightIndex)
+    		return null;
+        int max = -100000;
+        int maxindex = -1;
+        for(int i = leftIndex; i< rightIndex; i++) {
+        	if(nums[i]>max){
+        		max = nums[i];
+        		maxindex = i;
+        	}
+        }
+        //OK I got those values;
+        head = new TreeNode(max);        
+        head.left = null;
+        head.right = null;
+        head.left = constructMaximumBinaryTree(nums, leftIndex, maxindex, head.left);
+        head.right = constructMaximumBinaryTree(nums, maxindex+1, rightIndex, head.right);
+        return head;
+    }
