39ea0c611a2b8a5eec553ab9eea7c5398008542f
Hoang Cuong <hoangcuong2011@gmail.com>
2018-01-07 15:30:41 -0500

    Update FindAllDuplicatesinanArray.java

--- FindAllDuplicatesinanArray.java
+++ FindAllDuplicatesinanArray.java
@@ -16,3 +16,15 @@ public List<Integer> findDuplicates(int[] nums) {
     	}
     	return list;
     }
+
+//better solution, but it works only because elements >=1
+public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
